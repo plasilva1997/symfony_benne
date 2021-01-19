c6a0b3361198617bf422f7a001aeadf4e7bff9d5
Paulo Silva <paulosilva.pro97@gmail.com>
2021-01-19 16:08:47 +0100

    Api Puteaux

---
+++ src/Controller/ApiPuteauxController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiPuteauxController extends AbstractController
+{
+    /**
+     * @Route("/api/puteaux", name="api_puteaux")
+     */
+    public function index(): Response
+    {
+
+        $json = file_get_contents("https://ecoverre.paulosilva.xyz/benneverre.json");
+        $datas = json_decode($json);
+        dump($datas);
+
+
+        foreach ($datas as $data){
+
+            var_dump("La benne est situé :" . $data->fields->adresse);
+            echo '<br>';
+            var_dump("Jours de Collecte: " . $data->fields->frequence_de_collecte);
+            echo '<br>';
+            var_dump("Type de benne :" . $data->fields->type_colonne_aerien_enterre);
+            echo '<br>';
+            $coords = $data->fields->coordonnees_gps;
+            foreach ($coords as $coord){
+                var_dump($coord);
+            }
+
+            echo '<br>';
+            echo '<br>';
+
+        }
+
+        return $this->render('api_puteaux/index.html.twig', [
+            'controller_name' => 'ApiPuteauxController',
+        ]);
+    }
+}
