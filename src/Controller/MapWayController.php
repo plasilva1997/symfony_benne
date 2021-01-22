6f1f47d4c465f35c342d2b900697259625b47b75
AxelBarbeHusson <barbehussonaxel@gmail.com>
2021-01-22 16:23:05 +0100

    map done

--- src/Controller/MapWayController.php
+++ src/Controller/MapWayController.php
@@ -8,54 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MapWayController extends AbstractController
 {
-    private $client;
-
     /**
-     * @Route("/map/way", name="map_way")
+     * @Route("/map", name="map")
      */
-
-    public function fetchInformation(): array
-    {
-        $response = $this->client->request(
-            'GET',
-            'https://geo.api.gouv.fr/communes'
-        );
-
-        $statusCode = $response->getStatusCode();
-        // $statusCode = 200
-        $contentType = $response->getHeaders()['content-type'][0];
-        // $contentType = 'application/json'
-        $content = $response->getContent();
-        // $content = '{"id":521583, "name":"symfony-docs", ...}'
-        $content = $response->toArray();
-        // $content = ['id' => 521583, 'name' => 'symfony-docs', ...]
-
-        return $content;
-    }
-    public function Region(): Response
-    {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-        $entityManager = $this->getDoctrine()->getManager();
-
-        $product = new Product();
-        $product->setName();
-        $product->setPrice();
-        $product->setDescription();
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $entityManager->persist($product);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $entityManager->flush();
-
-        return new Response('Saved new product with id '.$product->getId());
-    }
     public function index(): Response
     {
         return $this->render('map_way/index.html.twig', [
-            'controller_name' => 'MapWayController',
-            'fetch' => 'fetchInformation',
+            'controller_name' => 'MapController',
         ]);
     }
 }
