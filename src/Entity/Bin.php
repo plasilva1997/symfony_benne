82cb2a8f197a5260d19a5894af296d00d769e2f8
Greugreu <clarke.victor76@hotmail.fr>
2021-01-22 15:05:05 +0100

    Creation of Bin entity

---
+++ src/Entity/Bin.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BinRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=BinRepository::class)
+ */
+class Bin
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $idBin;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $long;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $lat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $city;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $postalCode;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $collect;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $binType;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modifiedAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdBin(): ?int
+    {
+        return $this->idBin;
+    }
+
+    public function setIdBin(int $idBin): self
+    {
+        $this->idBin = $idBin;
+
+        return $this;
+    }
+
+    public function getLong(): ?float
+    {
+        return $this->long;
+    }
+
+    public function setLong(float $long): self
+    {
+        $this->long = $long;
+
+        return $this;
+    }
+
+    public function getLat(): ?float
+    {
+        return $this->lat;
+    }
+
+    public function setLat(float $lat): self
+    {
+        $this->lat = $lat;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getPostalCode(): ?int
+    {
+        return $this->postalCode;
+    }
+
+    public function setPostalCode(int $postalCode): self
+    {
+        $this->postalCode = $postalCode;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreeet(string $streeet): self
+    {
+        $this->streeet = $streeet;
+
+        return $this;
+    }
+
+    public function getCollect(): ?string
+    {
+        return $this->collect;
+    }
+
+    public function setCollect(string $collect): self
+    {
+        $this->collect = $collect;
+
+        return $this;
+    }
+
+    public function getBinType(): ?string
+    {
+        return $this->binType;
+    }
+
+    public function setBinType(string $binType): self
+    {
+        $this->binType = $binType;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getModifiedAt(): ?\DateTimeInterface
+    {
+        return $this->modifiedAt;
+    }
+
+    public function setModifiedAt(\DateTimeInterface $modifiedAt): self
+    {
+        $this->modifiedAt = $modifiedAt;
+
+        return $this;
+    }
+}
