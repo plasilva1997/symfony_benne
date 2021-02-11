b3e4cab8b557563c9809645ea1b59541e7bd8cf9
Paulo <paulosilva.pro97@gmail.com>
2021-02-11 10:29:29 +0100

    Legal fix

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class legal extends AbstractController
+class LegalController extends AbstractController
 {
     /**
      * @Route("/mentionslegales", name="legal")
