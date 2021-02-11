1317bc04bdc76d2367b872802039ae622672b245
Greugreu <clarke.victor76@hotmail.fr>
2021-02-11 10:53:54 +0100

    Fix LegalController Changed Toulouse API JSON to Lyon API JSON

--- src/Controller/APIController.php
+++ src/Controller/APIController.php
@@ -10,20 +10,22 @@ use Doctrine\ORM\EntityManager as em;
 class APIController extends AbstractController
 {
     /**
-     * @Route("/api/toulouse", name="ApiToulouse")
+     * @Route("/api/lyon", name="ApiLyon")
      * @return Response
      */
     public function dataJsonGet(): Response
     {
-        $json = file_get_contents("https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&facet=commune&facet=flux&facet=centre_ville&facet=prestataire&facet=zone&facet=pole");
+        $json = file_get_contents("https://download.data.grandlyon.com/wfs/grandlyon?SERVICE=WFS&VERSION=2.0.0&request=GetFeature&typename=gic_collecte.gicsiloverre&outputFormat=application/json; subtype=geojson&SRSNAME=EPSG:4171&startIndex=0&count=100");
         $datas = json_decode($json);
+
+        $bin = [];
 
         foreach ($datas as $data)
         {
 
         }
 
-        var_dump($data);
+
 
         return $this->render('map/mapview.html.twig', [
             'controller_name' => 'APIController',
