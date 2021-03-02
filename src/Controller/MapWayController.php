206eb736ecce4cfca5d990c5607c146f159a1e80
AxelBarbeHusson <barbehussonaxel@gmail.com>
2021-03-02 11:07:18 +0100

    change name

--- src/Controller/MapWayController.php
+++ src/Controller/MapWayController.php
@@ -6,14 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MapWayController extends AbstractController
+class MapController extends AbstractController
 {
     /**
      * @Route("/map", name="map")
      */
     public function index(): Response
     {
-        return $this->render('map_way/index.html.twig', [
+        return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
         ]);
     }
