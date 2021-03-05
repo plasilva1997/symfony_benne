4ee1240787c6b31ab701d63374391ec1b135f94c
plasilva1997 <72378847+plasilva1997@users.noreply.github.com>
2021-03-05 16:39:24 +0100

    map marker

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -4,21 +4,18 @@ namespace App\Controller;
 
 use App\Repository\BinRepository;
 use App\Service\SerializerService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class MapController extends AbstractController
 {
     private SerializerService $serializerService;
-    private EntityManagerInterface $em;
 
-    public function __construct(serializerService $serializer, EntityManagerInterface $entityManager)
-    {
-        $this->serializerService = $serializer;
-        $this->em = $entityManager;
+
+    public function __construct(SerializerService $serializerService){
+        $this->serializerService = $serializerService;
     }
 
     /**
@@ -28,12 +25,16 @@ class MapController extends AbstractController
      */
     public function index(BinRepository $binRepository): Response
     {
-        $query = $binRepository->findAll();
-        $json = JsonResponse::fromJsonString($this->serializerService->RelationSerializer($query, 'json'));
+        //recuperation donnes BDD
+        $bin = $binRepository->findAll();
+
+        //Seriallization
+        $jsonContent = $this->serializerService->serialize($bin);
+
 
         return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
-            'bins' => $json
+            'bins' => $jsonContent
         ]);
     }
 }
--- src/Service/SerializerService.php
+++ src/Service/SerializerService.php
@@ -1,54 +1,36 @@
 <?php
-
-
 namespace App\Service;
 
-
 use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Serializer;
 
 class SerializerService
 {
-    private array $encoders;
+    /**
+     * @var serializer
+     */
+    private $serializer;
 
+    /**
+     * EcoleController constructor.
+     */
     public function __construct()
     {
-        $this->encoders = [new XmlEncoder(), new JsonEncoder()];
-    }
-
-    public function RelationSerializer($inputData, $outputFormatData): string
-    {
-        $encoders = $this->encoders;
-
+        //Gestion des circulations
         $defaultContext = [
-            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object, $format, $context) {
+            AbstractNormalizer::CIRCULAR_REFERENCE_HANDLER => function ($object) {
                 return $object->getNom();
             },
         ];
+        $normalizer = new ObjectNormalizer(null, null, null, null, null, null, $defaultContext);
+        $this->serializer = new Serializer([$normalizer], [new JsonEncoder()]);
 
-        $normalizer = [new ObjectNormalizer(null, null, null, null, null, null, $defaultContext)];
-
-        $serializer = new Serializer($normalizer, $encoders);
-
-        $jsonContent = $serializer->serialize($inputData, $outputFormatData);
-
-        return $jsonContent;
     }
 
-    public function SimpleSerializer($inputData, $outFormatData): string
-    {
-        $encoders = $this->encoders;
-
-        $normalizers = [new ObjectNormalizer()];
-
-        $serializer= new Serializer($normalizers, $encoders);
-
-        $jsonContent = $serializer->serialize($inputData, $outFormatData);
-
-        return $jsonContent;
+    public function serialize($object):string{
+        return $this->serializer->serialize($object, 'json');
     }
 
 }
