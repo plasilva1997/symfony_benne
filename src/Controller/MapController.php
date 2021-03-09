ee475183c2853801a3f20d771ddc70bb7ac27e95
plasilva1997 <72378847+plasilva1997@users.noreply.github.com>
2021-03-09 10:03:15 +0100

    marker map not done

--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -4,9 +4,9 @@ namespace App\Controller;
 
 use App\Repository\BinRepository;
 use App\Service\SerializerService;
+use stdClass;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class MapController extends AbstractController
@@ -26,22 +26,42 @@ class MapController extends AbstractController
     public function index(BinRepository $binRepository): Response
     {
         //recuperation donnes BDD
-        $coords=[];
         $bins = $binRepository->findAll();
+        $geoJson = '{
+        "type": "FeatureCollection",
+        "features": [
+            
+                ]
+            }';
+        $geojson = json_decode($geoJson,true);
+
         foreach ($bins as $bin)
         {
-                array_push($coords, array($bin->getLat(), $bin->getLon(), $bin->getStreetNum(), $bin->getStreet(), $bin->getCity()));
+
+            $latLng = [$bin->getLon(),$bin->getLat()];
+            $tet = new stdClass();
+            $tet->type = "Feature";
+            $geo = new stdClass();
+            $geo->type = "Point";
+            $geo->coordinates =$latLng;
+            $tet->geometry =$geo;
+
+            $properties = new stdClass();
+            $properties->city = $bin->getCity();
+            $tet->properties = $properties;
+
+            array_push($geojson['features'],$tet);
+
         }
 
-        print_r($coords);
+//        $geojson=json_encode($geojson);
 
-        //Seriallization
-        $jsonContent = $this->serializerService->serialize($bin);
+
 
 
         return $this->render('map/index.html.twig', [
             'controller_name' => 'MapController',
-            'bins' => $coords
+            'bins' => $geojson
         ]);
     }
 }
